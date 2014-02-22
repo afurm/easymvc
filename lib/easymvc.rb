24b9c5a6ead3b0ac18d4edf674d6c290b39c2ad4
afurm <furmanets.andriy@gmail.com>
2014-02-22 18:46:09 +0200

    implemented simple route functionality

--- lib/easymvc.rb
+++ lib/easymvc.rb
@@ -3,7 +3,18 @@ require "easymvc/version"
 module Easymvc
   class Application
     def call(env)
-      [200, { "Content-Type" => "Text/html" }, ["Hello"]]
+      return [302, { "Location" => "/pages/about"}, []] if env["PATH_INFO"] == "/"
+      return [500, { "Location" => "/pages/about"}, []] if env["PATH_INFO"] == "/favicon.ico"
+      controller_class, action = get_controller_and_action(env)
+      response = controller_class.new.send(action)
+
+      [200, { "Content-Type" => "Text/html" }, [response]]
+    end
+
+    def get_controller_and_action(env)
+      _, controller_name, action = env["PATH_INFO"].split("/")
+      controller_name = controller_name.capitalize + "Controller"
+      [Object.const_get(controller_name), action]
     end
   end
 end
